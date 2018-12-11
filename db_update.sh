2e887c9190e03c7783e602b7cc27b3bba12c93d8
blubbfish <github.com@blubbfish.net>
2018-12-11 08:57:44 +0100

    Tiny Fixes

--- db_update.sh
+++ db_update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sqlite3 CoffeeDB.db -cmd "ALTER TABLE user ADD startmoney type float" -batch
-sqlite3 CoffeeDB.db -cmd "CREATE TABLE `cashdesk` (`cashid`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,	`price`	FLOAT NOT NULL DEFAULT 0.0,	`date`	DATETIME NOT NULL,	`item`	TEXT NOT NULL);" -batch
+sqlite3 -batch CoffeeDB.db "ALTER TABLE user ADD startmoney type float"
+sqlite3 -batch CoffeeDB.db "CREATE TABLE \`cashdesk\` (\`cashid\` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \`price\` FLOAT NOT NULL DEFAULT 0.0, \`date\` DATETIME NOT NULL, \`item\` TEXT NOT NULL);"
 mv CoffeeDB.db SnackBar/
